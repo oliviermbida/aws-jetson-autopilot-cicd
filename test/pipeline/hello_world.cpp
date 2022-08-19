e2ded1532485cb1853900c1535b5f4c8d7c236ea
Olivier Mbida <olivier.mbida@ai-uavsystems.com>
2022-08-19 13:38:02 +0000

    Test g++ build with self-hosted dockeragent in azure pipeline

---
+++ test/pipeline/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+/// @Brief Test Azure pipoeline agent
+/// @return Return exit status
+int main()
+{
+  std::cout << "Hello, World! Compiled from a self hosted docker container agent using azure pipeline." << std::endl;
+  return 0;
+}
